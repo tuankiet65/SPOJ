24216677981259d4c18df633da0717d87a9c1f55
Ho Tuan Kiet <tuankiet65@gmail.com>
2015-06-18 11:24:38 +0700

    Push submission ID 11138978 for problem MYSTERY

---
+++ MYSTERY/11138978-src.pas
@@ -0,0 +1,24 @@
+program mystery;
+uses math;
+const m=20122007;
+var n, t: int64;
+function expo(n: int64): int64;
+ begin
+  if n<=39 then expo:=(3**n) mod m else
+   expo:=((expo(n div 2) mod m)**2*ifthen(n mod 2<>0, 3, 1)) mod m;
+ end;
+procedure u(n: int64);
+ var i: longint;
+ begin
+  for i:=1 to trunc(sqrt(n)) do
+   if (n mod i=0) then begin
+    t:=(t*((expo(i)-1) mod m)) mod m;
+    if i<>sqrt(n) then t:=(t*((expo(n div i)-1) mod m)) mod m;
+   end;
+ end;
+begin
+ t:=1;
+ readln(n);
+ u(n);
+ write(t);
+end.
