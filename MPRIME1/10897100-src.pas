b9a1649f2f649163d374a28041702fe50680c9e9
Ho Tuan Kiet <tuankiet65@gmail.com>
2015-06-18 11:26:38 +0700

    Push submission ID 10897100 for problem MPRIME1

---
+++ MPRIME1/10897100-src.pas
@@ -0,0 +1,45 @@
+program mprime1;
+var a: array [1..2464] of longint;
+    n: longint;
+function isprime(n: longint): boolean;
+ var i: longint;
+ begin
+  if n=1 then exit(false);
+  for i:=2 to trunc(sqrt(n)) do
+   if n mod i=0 then exit(false);
+  exit(true);
+ end;
+procedure sinh;
+ var i, i2: longint;
+ begin
+  i2:=0;
+  for i:=2 to 22000 do
+   if isprime(i) then begin
+    inc(i2);
+    a[i2]:=i;
+   end;
+ end;
+function find(n: longint): longint;
+ var i, i2, i3: longint;
+     tmp: qword;
+ begin
+  tmp:=0;
+  find:=0;
+  i2:=0;
+  for i:=1 to 1334 do begin
+   i2:=i; 
+   while tmp<n do begin
+    tmp:=tmp+a[i2];
+    inc(i2);
+   end;
+   if tmp=n then inc(find);
+   tmp:=0;
+  end;
+ end;
+begin
+ sinh;
+ repeat
+  readln(n);
+  if n<>0 then writeln(find(n));
+ until n=0;
+end.
