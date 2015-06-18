b9be6ebe6a65380fcb37ed0818544e41e46dc6f6
Ho Tuan Kiet <tuankiet65@gmail.com>
2015-06-18 11:23:37 +0700

    Push submission ID 11290551 for problem MAXARR1

---
+++ MAXARR1/11290551-src.pas
@@ -0,0 +1,29 @@
+program maxarr1;
+var a, b: array [0..100000] of int64;
+    t, gend, n, i: longint;
+procedure gene(x, y: longint);
+ var i: longint;
+ begin
+  for i:=x to y do begin
+   a[i]:=a[i div 2];
+   if i mod 2<>0 then a[i]:=a[i]+a[(i div 2)+1];
+   if a[i]>b[i-1] then b[i]:=a[i] else b[i]:=b[i-1];
+  end;
+ end;
+begin
+ a[0]:=0;
+ a[1]:=1;
+ b[0]:=0;
+ b[1]:=1;
+ gend:=2;
+ readln(t);
+ for i:=1 to t do begin
+  readln(n);
+  if n>gend then begin
+   gene(gend, n);
+   inc(gend, n+1);
+  end;
+  writeln(b[n]);
+ end;
+end.
+
