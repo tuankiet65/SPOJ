9421a056ddb45a19cf5b99af735245709e67d134
Ho Tuan Kiet <tuankiet65@gmail.com>
2015-06-18 11:18:12 +0700

    Push submission ID 13822970 for problem COUNTCBG

---
+++ COUNTCBG/13822970-src.pas
@@ -0,0 +1,23 @@
+program countcbg;
+var n: longint;
+    t: text;
+function process(n: longint): longint;
+ var x: longint;
+     y: extended;
+ begin
+  process:=0;
+  for x:=1 to (n div 2) do begin
+   y:=n/(x+1)-(x/2);
+   if trunc(y)=0 then exit();
+   if trunc(y)=y then inc(process);
+  end;
+ end;
+begin
+ assign(t, '');
+ reset(t);
+ while not eof(t) do begin
+  readln(t, n);
+  writeln(process(n));
+ end;
+ close(t);
+end.
