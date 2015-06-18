70c221b70225c5a639d10aef3c988d3c79005c7a
Ho Tuan Kiet <tuankiet65@gmail.com>
2015-06-18 11:19:27 +0700

    Push submission ID 13307874 for problem VRATF

---
+++ VRATF/13307874-src.pas
@@ -0,0 +1,16 @@
+program vratf;
+var n, k: longint;
+    d: qword; 
+procedure try(n: longint);
+ begin
+  if ((n+k) mod 2=0) and (((n+k) div 2)-k<>0) then begin
+   try((n+k) div 2);
+   try(((n+k) div 2)-k);
+  end else inc(d);
+ end;
+begin
+ d:=0;
+ readln(n, k);
+ try(n);
+ writeln(d);
+end.
