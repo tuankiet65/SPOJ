511bc3df14db307be5f20d18fb2dbf3e8d98c7e7
Ho Tuan Kiet <tuankiet65@gmail.com>
2015-06-18 11:24:21 +0700

    Push submission ID 11152912 for problem QBSEQ

---
+++ QBSEQ/11152912-src.pas
@@ -0,0 +1,20 @@
+program qbseq;
+var a: array [1..1000] of longint;
+    b: array [0..1000] of qword;
+    n, i, k, max, i2: longint;
+begin
+ readln(n, k);
+ b[0]:=0;
+ for i:=1 to n do begin
+  read(a[i]);
+  b[i]:=b[i-1]+a[i];
+ end;
+ if (k<=0) then begin
+  write(0);
+  exit;
+ end; 
+ for i:=1 to n do
+  for i2:=i to n do
+   if ((b[i2]-b[i-1]) mod k=0) and (i2-i+1>max) then max:=i2-i+1;
+ write(max);
+end.
