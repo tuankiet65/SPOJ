a57e3559f4317b75cd01dac57286a422e80cc578
Ho Tuan Kiet <tuankiet65@gmail.com>
2015-06-18 11:33:47 +0700

    Push submission ID 10609532 for problem MULONE

---
+++ MULONE/10609532-src.pas
@@ -0,0 +1,13 @@
+var k, i, s, i2, x: longint;
+begin
+ readln(k);
+ for i:=1 to k do begin
+  readln(s);
+  for i2:=1 to (s-1) div 9 do write('123456790');
+  x:=(s-1) mod 9;
+  for i2:=1 to x do write(i2);
+  for i2:=x+1 downto 2 do write(i2);
+  for i2:=1 to (s-1) div 9 do write('098765432');
+  writeln(1);
+ end;
+end.
