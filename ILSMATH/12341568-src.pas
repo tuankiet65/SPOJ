f7b501a3e7dba5ae49439ce042b878498a0c5e4a
Ho Tuan Kiet <tuankiet65@gmail.com>
2015-06-18 11:21:55 +0700

    Push submission ID 12341568 for problem ILSMATH

---
+++ ILSMATH/12341568-src.pas
@@ -0,0 +1,38 @@
+program ilsmath;
+uses math;
+var i, ca: longint;
+    res: extended;
+    a: array [1..100] of longint;
+    n: int64;
+function ceil_custom(n: extended): int64;
+ begin
+  ceil_custom:=trunc(n);
+  if frac(n)>0 then inc(ceil_custom);
+ end;
+begin
+ fillchar(a, sizeof(a), 0);
+ ca:=0;
+ readln(n);
+ res:=0;
+ i:=0;
+ if n<0 then begin
+  write('NO');
+  exit;
+ end;
+ while ceil_custom(res)<n do begin
+  inc(i);
+  res:=res+log10(i);
+ end;
+ while ceil_custom(res)=n do begin
+  inc(ca);
+  a[ca]:=i;
+  inc(i);
+  res:=res+log10(i);
+ end;
+ if ca=0 then
+  write('NO')
+ else begin
+  writeln(ca);
+  for i:=1 to ca do writeln(a[i]);
+ end; 
+end.
