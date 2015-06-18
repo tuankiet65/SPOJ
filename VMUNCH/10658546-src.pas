425f788bdc33abe401223a4c4a44480616a39fc7
Ho Tuan Kiet <tuankiet65@gmail.com>
2015-06-18 11:31:04 +0700

    Push submission ID 10658546 for problem VMUNCH

---
+++ VMUNCH/10658546-src.pas
@@ -0,0 +1,42 @@
+const x: array [1..4] of integer=(-1,0,1,0);
+      y: array [1..4] of integer=(0,-1,0,1);
+var r, c, i, i2, fr, fc, min: longint;
+    map: array [1..100, 1..100] of char;
+    mapb: array [0..100, 0..100] of boolean;
+    s: string;
+procedure try(f, t, k: longint);
+ var i: longint;
+ begin
+  for i:=1 to 4 do
+   if (mapb[f+x[i], t+y[i]]=false) and (map[f+x[i], t+y[i]]<>'*') then begin
+    mapb[f+x[i], t+y[i]]:=true;
+    if map[f+x[i], t+y[i]]='B' then begin
+      write(k);
+      exit;
+    end else try(f+x[i], t+y[i], k+1);
+    mapb[f+x[i], t+y[i]]:=false;
+   end;
+ end;
+begin
+ fillchar(mapb, sizeof(mapb), false);
+ readln(r, c);
+ for i:=0 to c+1 do begin
+  mapb[0][i]:=true;
+  mapb[r+1][i]:=true;
+ end;
+ for i:=1 to r do begin
+  mapb[i][0]:=true;
+  mapb[i][c+1]:=true;
+  readln(s);
+  for i2:=1 to c do begin
+   map[i][i2]:=s[i2];
+   if s[i2]='C' then begin
+    fr:=i;
+    fc:=i2;
+   end;
+  end;
+ end;
+ if (fr<>0) and (fc<>0) then try(fr, fc, 1);
+end.
+
+
