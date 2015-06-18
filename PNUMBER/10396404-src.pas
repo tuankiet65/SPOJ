470dfa6dca80975aac9719f652262d9b47ca1d97
Ho Tuan Kiet <tuankiet65@gmail.com>
2015-06-18 11:37:00 +0700

    Push submission ID 10396404 for problem PNUMBER

---
+++ PNUMBER/10396404-src.pas
@@ -0,0 +1,20 @@
+program prime;
+uses crt;
+var a,b, i: longint;
+function prime(n: longint): boolean;
+ var i: longint;
+ begin
+  prime:=true;
+  for i:=2 to round(sqrt(n)) do
+   if n mod i=0 then begin
+    prime:=false;
+    exit;
+   end;
+ end;
+begin
+ clrscr;
+ read(a,b);
+ for i:=a to b do
+  if prime(i)=true then
+   writeln(i);
+end.
