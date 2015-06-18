94ff5eefb0d4dc5bb0b3f522508afaeb0d54d044
Ho Tuan Kiet <tuankiet65@gmail.com>
2015-06-18 11:19:25 +0700

    Push submission ID 13307957 for problem VRATF

---
+++ VRATF/13307957-src.pas
@@ -0,0 +1,15 @@
+program vratf;
+var n, k, d: longint;
+procedure try(n: longint);
+ begin
+  if ((n-k) mod 2=0) and (n<>k) then begin
+   try((n-k) div 2);
+   try(((n-k) div 2)+k);
+  end else inc(d);
+ end;
+begin
+ d:=0;
+ readln(n, k);
+ try(n);
+ writeln(d);
+end.
