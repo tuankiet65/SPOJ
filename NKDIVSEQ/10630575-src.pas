ec7352903f4bfdc78f6895332c5f49938e02e78b
Ho Tuan Kiet <tuankiet65@gmail.com>
2015-06-18 11:31:28 +0700

    Push submission ID 10630575 for problem NKDIVSEQ

---
+++ NKDIVSEQ/10630575-src.pas
@@ -0,0 +1,13 @@
+var n, d, i, max: qword;
+begin
+ readln(n);
+ d:=1;
+ i:=1;
+ max:=1;
+ while d<n do begin
+  inc(i);
+  inc(d, i);
+  max:=(max+(i-(max mod i)))+(i*(i-1));
+ end;
+ write(max-(i*(d-n)));
+end.
