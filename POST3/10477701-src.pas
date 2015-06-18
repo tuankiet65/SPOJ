d3f6b50d47bda0b753345b720cd1f19092f1629e
Ho Tuan Kiet <tuankiet65@gmail.com>
2015-06-18 11:36:16 +0700

    Push submission ID 10477701 for problem POST3

---
+++ POST3/10477701-src.pas
@@ -0,0 +1,12 @@
+program congcongcongcongcongcongcongcongcongcongcongcongcongcong;
+var a,      b:   longint;
+    c: int64;
+    d, e, f, g, h, i, j, k, l, m, n, o, p, q, r, s, t, u, v, x, y, z, aa, ab, ac, ad: byte;
+begin
+ read(a);
+ read(b);
+ c:=(a+b);
+ write(c);
+end.
+
+
