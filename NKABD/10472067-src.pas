d1c9d260417fff99a23b283c5ad04aec86d5bdc2
Ho Tuan Kiet <tuankiet65@gmail.com>
2015-06-18 11:36:43 +0700

    Push submission ID 10472067 for problem NKABD

---
+++ NKABD/10472067-src.pas
@@ -0,0 +1,21 @@
+program nkabd;
+uses crt;
+var l, r, i, d: longint;
+function pp(n: longint): longint;
+ var i, t: longint;
+ begin
+  t:=0;
+  if (n=1) or (n=0) then exit(0);
+  for i:=1 to trunc(sqrt(n)) do
+   if n mod i=0 then t:=t+i+(n div i);
+ pp:=t-n;
+ if i=sqrt(n) then pp:=pp-i;
+ end;
+begin
+ clrscr;
+ read(l, r);
+ for i:=l to r do
+  if pp(i)>i then
+   inc(d);
+ write(d);
+end.
