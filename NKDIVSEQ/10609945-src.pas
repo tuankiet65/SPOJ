f249316923e0222b446f54911cc6b4b717aac056
Ho Tuan Kiet <tuankiet65@gmail.com>
2015-06-18 11:33:32 +0700

    Push submission ID 10609945 for problem NKDIVSEQ

---
+++ NKDIVSEQ/10609945-src.pas
@@ -0,0 +1,16 @@
+program nkdivseq;
+var n, d, i, max: longint;
+begin
+ readln(n);
+ d:=1;
+ i:=1;
+ max:=1;
+ while d<n do begin
+  inc(i);
+  d:=d+i;
+  max:=max+(i-(max mod i));
+  max:=max+(i*(i-1));
+ end;
+ if d=n then write(max) else
+  write(max-(i*(d-n)));
+end.
