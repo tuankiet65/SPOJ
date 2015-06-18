3a4f5251c2d3063da9bee3d5a8ea6bb0d984485a
Ho Tuan Kiet <tuankiet65@gmail.com>
2015-06-18 11:25:34 +0700

    Push submission ID 10923622 for problem QBMAX

---
+++ QBMAX/10923622-src.pas
@@ -0,0 +1,27 @@
+program qbmax;
+var a, b: array [0..100, 1..100] of longint;
+    m, n, i, i2, x: longint; 
+function max(a, b, c: longint): longint;
+ begin
+  max:=a;
+  if b>max then max:=b;
+  if c>max then max:=c;
+ end;
+begin
+ readln(m, n);
+ for i:=1 to m do
+  for i2:=1 to n do read(a[i][i2]);
+ for i:=1 to m do
+  b[i][1]:=a[i][1];
+ for i:=1 to n do begin
+  b[0][i]:=-maxlongint;
+  b[m+1][i]:=-maxlongint;
+ end;
+ for i2:=2 to n do
+  for i:=1 to m do
+   b[i][i2]:=a[i][i2]+max(b[i][i2-1], b[i+1][i2-1], b[i-1][i2-1]);
+ x:=b[1][n];
+ for i:=2 to m do
+  if b[i][n]>x then x:=b[i][n];
+ write(x);
+end.
