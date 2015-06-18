8caeea67bf37f94e44a9fe550c025d2441e9a6c3
Ho Tuan Kiet <tuankiet65@gmail.com>
2015-06-18 11:24:56 +0700

    Push submission ID 10938287 for problem C11KM

---
+++ C11KM/10938287-src.pas
@@ -0,0 +1,28 @@
+program c11km;
+uses crt;
+var n, i, t: longint;
+    a: array [1..1000] of longint;
+procedure findmax(n: longint);
+ var i, tmp1, tmp2: longint;
+ begin
+  tmp1:=0;
+  tmp2:=0;
+  for i:=1 to n do
+   if (a[i]>tmp2) and (a[i]<>n) then begin
+    tmp1:=i;
+    tmp2:=a[i];
+   end;
+  a[tmp1]:=0;
+ end;
+begin
+ clrscr;
+ readln(n);
+ for i:=1 to n do
+  readln(a[i]);
+ for i:=1 to n do begin
+  t:=t+a[i];
+  if a[i]>=100 then findmax(a[i]);
+  a[i]:=0;
+ end;
+ write(t);
+end.
